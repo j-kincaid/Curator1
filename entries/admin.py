698bb86e61d74dbb47ca0917798ad6a9bb16860f
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-05 14:44:27 -0500

    create ManyToManyField relationships

--- entries/admin.py
+++ entries/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import Artwork
+from .models import Artwork, Comment, Tag
 from . import models
 
-class ArtworkAdmin(admin.ModelAdmin):
-    list_display = ('artwork_title',)
+# class ArtworkAdmin(admin.ModelAdmin):
+#     list_display = ('artwork_title',)
 
 # class ArtistAdmin(admin.ModelAdmin):
 #     list_display = ('full_name',)
 
-# admin.site.register(models.Artist, ArtistAdmin)
-admin.site.register(models.Artwork, ArtworkAdmin)
+admin.site.register(models.Artwork)
+# admin.site.register(models.Artist)
+admin.site.register(models.Comment)
+admin.site.register(models.Tag)
--- entries/models.py
+++ entries/models.py
@@ -1,24 +1,33 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-
-class Artist(models.Model):
-    full_name = models.CharField(max_length=200)
-    pronouns = models.CharField(max_length=200)
-    bio = models.TextField()
-    pub_date = models.DateTimeField('date published')
+import uuid
 
 # Each Artwork is related to a single artist. 
 class Artwork(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    votes = models.IntegerField(default=0)
+    # artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    # votes = models.IntegerField(default=0)
     artwork_title = models.CharField(max_length=200)
     artwork_medium = models.CharField(max_length=200, default="ex. Fiber")
+    # tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_average = models.IntegerField(default=0, null=True, blank=True)
     height_in_inches = models.DecimalField(max_digits=5, decimal_places=2, default="inches")
     width_in_inches = models.DecimalField(max_digits=5, decimal_places=2, default="inches")
     depth_in_inches = models.DecimalField(max_digits=5, decimal_places=2, default="inches")
     year_completed = models.IntegerField(default=0)
+    # The following line produced a ValueError "badly formed hexadecimal UUID string"
+    # id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    def __str__(self):
+        return self.artwork_title
 
+
+# class Artist(models.Model):
+#     full_name = models.CharField(max_length=200)
+#     pronouns = models.CharField(max_length=200)
+#     bio = models.TextField()
+#     pub_date = models.DateTimeField('date published')
+#     def __str__(self):
+#         return self.full_name
 
 # Trying to add this subclass as per docs unsuccessful 4/24/22 2:31PM
 
@@ -31,4 +40,27 @@ class Artwork(models.Model):
     #     PRINT = 'PT', _('Print')
     #     DIGITAL = 'DG', _('Digital')
     #     OTHER = 'OT', _('Other')
+
+class Comment(models.Model):
+    # artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
+    # panelist =
+
+    artworks = models.ManyToManyField('Artwork', blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    def __str__(self):
+           return self.value
+
+class Tag(models.Model):
+    artwork = models.ForeignKey(Artwork, null=True, on_delete=models.CASCADE)
+    entry_score_1_to_5 = models.DecimalField(default=0, max_digits=1, decimal_places=0)
+    pub_date = models.DateTimeField('date published')
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+
+
+
+
     
