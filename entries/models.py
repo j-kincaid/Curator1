b2073bb029fd9e1f8deb2d23af965000195afd74
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-05 16:21:22 -0500

    delete string from tag

--- entries/models.py
+++ entries/models.py
@@ -4,7 +4,6 @@ import uuid
 
 # Each Artwork is related to a single artist. 
 class Artwork(models.Model):
-    # artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     # votes = models.IntegerField(default=0)
     artwork_title = models.CharField(max_length=200)
     artwork_medium = models.CharField(max_length=200, default="ex. Fiber")
@@ -21,13 +20,14 @@ class Artwork(models.Model):
         return self.artwork_title
 
 
-# class Artist(models.Model):
-#     full_name = models.CharField(max_length=200)
-#     pronouns = models.CharField(max_length=200)
-#     bio = models.TextField()
-#     pub_date = models.DateTimeField('date published')
-#     def __str__(self):
-#         return self.full_name
+class Artist(models.Model):
+    full_name = models.CharField(max_length=200)
+    pronouns = models.CharField(max_length=200)
+    bio = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    artworks = models.ManyToManyField('Artwork', blank=True)
+    def __str__(self):
+        return self.full_name
 
 # Trying to add this subclass as per docs unsuccessful 4/24/22 2:31PM
 
@@ -43,21 +43,22 @@ class Artwork(models.Model):
 
 class Comment(models.Model):
     # artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
-    # panelist =
 
     artworks = models.ManyToManyField('Artwork', blank=True)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=500)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     def __str__(self):
            return self.value
 
 class Tag(models.Model):
-    artwork = models.ForeignKey(Artwork, null=True, on_delete=models.CASCADE)
+    artwork = models.ForeignKey('Artwork', null=True, on_delete=models.CASCADE)
     entry_score_1_to_5 = models.DecimalField(default=0, max_digits=1, decimal_places=0)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    # def __str__(self):
+    #        return self.artwork
     
 
 
