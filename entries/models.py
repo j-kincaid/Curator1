ccbaf639ce301622fbc12c63cc26a10165013e37
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-06 10:30:26 -0500

    add choices of media to artwork form

--- entries/models.py
+++ entries/models.py
@@ -6,16 +6,26 @@ import uuid
 class Artwork(models.Model):
     # votes = models.IntegerField(default=0)
     artwork_title = models.CharField(max_length=200)
-    artwork_medium = models.CharField(max_length=200, default="ex. Fiber")
     # tags = models.ManyToManyField('Tag', blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_average = models.IntegerField(default=0, null=True, blank=True)
+
+    MEDIA = (
+        ('PA', 'Painting'),
+        ('SC', 'Sculpture'),
+        ('PH', 'Photography'),
+        ('CE', 'Ceramic'),
+        ('TX', 'Textile'),
+        ('PT','Print'),
+        ('DG', 'Digital'),
+        ('OT','Other'),
+    )
+    medium = models.CharField(max_length=2, null=True, choices=MEDIA)
+    
     height_in_inches = models.DecimalField(max_digits=5, decimal_places=2, default="inches")
     width_in_inches = models.DecimalField(max_digits=5, decimal_places=2, default="inches")
     depth_in_inches = models.DecimalField(max_digits=5, decimal_places=2, default="inches")
-    year_completed = models.IntegerField(default=0)
-    # The following line produced a ValueError "badly formed hexadecimal UUID string"
-    # id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    year_completed = models.IntegerField(default=2022)
     def __str__(self):
         return self.artwork_title
 
@@ -31,15 +41,7 @@ class Artist(models.Model):
 
 # Trying to add this subclass as per docs unsuccessful 4/24/22 2:31PM
 
-    # class Medium(models.TextChoices):
-    #     PAINTING = 'PA', _('Painting')
-    #     SCULPTURE = 'SC', _('Sculpture')
-    #     PHOTOGRAHY = 'PH', _('Photography')
-    #     CERAMIC = 'CE', _('Ceramic')
-    #     TEXTILE = 'TX', _('Textile')
-    #     PRINT = 'PT', _('Print')
-    #     DIGITAL = 'DG', _('Digital')
-    #     OTHER = 'OT', _('Other')
+
 
 class Comment(models.Model):
     # artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
@@ -57,8 +59,6 @@ class Tag(models.Model):
     entry_score_1_to_5 = models.DecimalField(default=0, max_digits=1, decimal_places=0)
     pub_date = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    # def __str__(self):
-    #        return self.artwork
     
 
 
