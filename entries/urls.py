39491cf8534b6c9ce70429fe73ce9a7ea8ec9daf
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-07 10:23:26 -0500

    add subclass to Artwork object

--- entries/urls.py
+++ entries/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('entries', views.EntriesListView.as_view(), name="entries.list"),
     path('entries/<int:pk>', views.EntriesDetailView.as_view(), name="entries.detail"),
--- entries/views.py
+++ entries/views.py
@@ -1,15 +1,17 @@
-from typing import List
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView
 
-
+from .forms import EntriesForm
 from .models import Artwork
 
 class EntriesCreateView(CreateView):
     model = Artwork
+    fields = ['artwork_title', 'medium', 'vote_total', 'vote_average', 'height_in_inches', 'width_in_inches', 'depth_in_inches', 'year_completed']
+    success_url = 'artballot/artworks'
+    form_class = EntriesForm
     template_name = "entries/entries_form.html"
-    context_object_name = "entries"
+    # context_object_name = "entries"
     
 class EntriesListView(ListView):
     model = Artwork
