a10583d66a20ec665e937e7d4e1a15a28e7e5b41
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-09 16:04:29 -0500

    Add entries.detail to models.py

--- entries/urls.py
+++ entries/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from . import views
-
+from entries.views import EntriesListView, EntriesDetailView, EntriesCreateView, EntriesUpdateView, EntriesDeleteView
 
 urlpatterns = [
-    path('entries', views.EntriesListView.as_view(), name="entries.list"),
-    path('entries/<int:pk>', views.EntriesDetailView.as_view(), name="entries.detail"),
-    path('entries/new/', views.EntriesCreateView.as_view(), name="entries.new"),
-    path('entries/<int:pk>/update/', views.EntriesUpdateView.as_view(), name="entries.update"),
-    path('entries/<int:pk>/delete/', views.EntriesDeleteView.as_view(), name="entries.delete"),
+    path('entries', EntriesListView.as_view(), name="entries.list"),
+    path('entries/<int:pk>', EntriesDetailView.as_view(), name="entries.detail"),
+    path('entries/form/', EntriesCreateView.as_view(), name='entries.form'),
+    path('entries/<int:pk>/', EntriesUpdateView.as_view(), name='entries.update'),
+    path('entries/<int:pk>/delete/', EntriesDeleteView.as_view(), name='entries.delete'),
 ]
