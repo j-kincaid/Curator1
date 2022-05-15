994593f34e40e6e35f948bf874f00561642cc6d0
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-15 15:06:33 -0500

    add icon for new entry

--- entries/forms.py
+++ entries/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 
-class EntriesForm(forms.Form):
-    name = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea)
+from .models import Artwork
+
+class ArtworkForm(forms.ModelForm):
+    class Meta:
+        model = Artwork
+        fields = ('artwork_title', 'medium', 'height_in_inches', 'width_in_inches', 'depth_in_inches', 'year_completed',)
+    # name = forms.CharField()
+    # message = forms.CharField(widget=forms.Textarea)
 
