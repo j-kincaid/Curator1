513c92316b674fe5a253ba774b8e3bea28762b97
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-25 16:32:59 -0500

    add bootstrap, reconnect edit template

--- entries/urls.py
+++ entries/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('entries', EntriesListView.as_view(), name="entries.list"),
     path('entries/<int:pk>', EntriesDetailView.as_view(), name="entries.detail"),
     path('entries/form/', EntriesCreateView.as_view(), name='entries.form'),
-    path('entries/<int:pk>/edit', EntriesUpdateView.as_view(), name='entries.edit'),
+    path('entries/<int:pk>/edit', EntriesUpdateView.as_view(), name='entries.update'),
     path('entries/<int:pk>/delete/', EntriesDeleteView.as_view(), name='entries.delete'),
 ]
--- entries/views.py
+++ entries/views.py
@@ -1,11 +1,12 @@
-from dataclasses import field
 from django.http import Http404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.forms import ModelForm
-from .models import Artwork
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .models import Artwork
+from .forms import EntriesForm
 
 class EntriesListView(ListView):
     model = Artwork
@@ -13,42 +14,40 @@ class EntriesListView(ListView):
     template_name = "entries/entries_list.html"
 
 
-class EntriesDetailView(DetailView):
+class EntriesDetailView(LoginRequiredMixin, DetailView):
     model = Artwork
     context_object_name = "entry"
     template_name = "entries/entries_detail.html"
+    login_url = '/login'
 
 """
 Using ModelForm for database-driven features. The docs say 'You do not even need to provide a success_url for CreateView or UpdateView - they will use get_absolute_url() on the model object if available'.
 
 """
-class EntriesForm(ModelForm):
+class EntriesForm(LoginRequiredMixin, ModelForm):
     class Meta:
         model = Artwork
         fields = ['artwork_title', 'medium', 'height_in_inches', 'width_in_inches', 'depth_in_inches', 'year_completed']
+        login_url = '/login'
 
 
-class EntriesCreateView(CreateView):
+class EntriesCreateView(LoginRequiredMixin, CreateView):
     model = Artwork
     template_name = 'entries/entries_edit.html'
     fields = ['artwork_title', 'medium', 'height_in_inches', 'width_in_inches', 'depth_in_inches', 'year_completed']
+    login_url = '/login'
 
 
-class EntriesUpdateView(UpdateView):
+class EntriesUpdateView(LoginRequiredMixin, UpdateView):
     model = Artwork
-    fields = ['artwork_title', 'medium', 'height_in_inches', 'width_in_inches', 'depth_in_inches', 'year_completed']
+    success_url: reverse_lazy('entries_edit')
+    form_class = EntriesForm
+    login_url = '/login'
 
 
-class EntriesDeleteView(DeleteView):
+class EntriesDeleteView(LoginRequiredMixin, DeleteView):
     model = Artwork
-    # success_url = reverse_lazy('entries_list')
-
-    success_url = '/' # when post is deleted, redirect user to Homepage
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
+    success_url = reverse_lazy('entries_list')
+    template_engine = '/entries/entries_delete.html'
+    login_url = '/login'
 
