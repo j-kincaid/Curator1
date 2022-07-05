197433bf0e20e34280d9a8ab3e03299a0e86f9a4
Jessica Kincaid <4j.kincaid@gmail.com>
2022-07-05 17:08:06 -0500

    add link to voting from artwork detail

--- entries/admin.py
+++ entries/admin.py
@@ -5,10 +5,4 @@ from . import models
 class ArtworkAdmin(admin.ModelAdmin):
     list_display = ('artwork_title',)
 
-# class ArtistAdmin(admin.ModelAdmin):
-#     list_display = ('full_name',)
-
 admin.site.register(models.Artwork, ArtworkAdmin)
-# admin.site.register(models.Artist)
-# admin.site.register(models.Comment)
-# admin.site.register(models.Tag)
