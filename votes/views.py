b1c647faeb996e8e5701e415d94420c4bc2d1ddb
Jessica Kincaid <4j.kincaid@gmail.com>
2022-07-06 14:36:06 -0500

    update UX

--- votes/views.py
+++ votes/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django .views import generic 
+from django.views import generic 
 from django.utils import timezone
 
 from .models import Choice, Question
@@ -11,10 +11,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
         return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+        pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     
 class DetailView(generic.DetailView):
     model = Question
@@ -42,7 +44,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse('votes:results', args=(question.id,)))
