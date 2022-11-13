5dea9e56a02e155decbccdd428976d678acfbd00
Jessica Kincaid <4j.kincaid@gmail.com>
2022-11-13 15:15:03 -0600

    tidy up views, admin

--- votes/views.py
+++ votes/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -16,7 +16,7 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+        return Choice.objects.filter(pub_date__lte=timezone.now()).order_by(
             "-pub_date"
         )[:5]
 
