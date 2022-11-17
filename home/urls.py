b69afae173caaf8044d68b508e541a80e6311a2b
Jessica Kincaid <4j.kincaid@gmail.com>
2022-11-17 14:14:59 -0600

    delete forst votes app

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+# from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
+    # path('admin', admin.site.urls),
+    path('', include('votes')), 
     path("", views.HomeView.as_view(), name="home"),
     path("login", views.LoginInterfaceView.as_view(), name="login"),
     path("logout", views.LogoutInterfaceView.as_view(), name="logout"),
