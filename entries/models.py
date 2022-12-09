41971fee4ed5062c0dff89f18707bcf1c66aa2a1
Jessica Kincaid <4j.kincaid@gmail.com>
2022-12-09 16:08:13 -0600

    add description and votes to Artwork model

--- entries/models.py
+++ entries/models.py
@@ -36,7 +36,13 @@ class Artwork(models.Model):
     )
     year_completed = models.IntegerField(default=2022)
 
+    description = models.TextField(null=True, blank=True)
+
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField("Tag", blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.artwork_title
@@ -44,29 +50,6 @@ class Artwork(models.Model):
     def get_absolute_url(self):
         return reverse("entries.detail", kwargs={"pk": self.pk})
 
-
-# class Artist(models.Model):
-#     full_name = models.CharField(max_length=200)
-#     pronouns = models.CharField(max_length=200)
-#     bio = models.TextField()
-#     pub_date = models.DateTimeField("date published")
-#     artworks = models.ManyToManyField("Artwork", blank=True)
-
-#     def __str__(self):
-#         return self.full_name
-
-
-# class Comment(models.Model):
-#     artworks = models.ManyToManyField("Artwork", blank=True)
-#     body = models.TextField(null=True, blank=True)
-#     value = models.CharField(max_length=500)
-#     pub_date = models.DateTimeField(auto_now_add=True)
-#     id = models.UUIDField(
-#         default=uuid.uuid4, unique=True, primary_key=True, editable=False
-#     )
-
-#     def __str__(self):
-#         return self.value
 
 
 class Review(models.Model):
